238d5b4bd80906524cd1264d5d29b68494c346e9
jigar b. patel <jigarpatel5885@gmail.com>
2022-09-04 19:32:00 +0530

    referencing component

--- TangyWeb_Server/wwwroot/js/Common.js
+++ TangyWeb_Server/wwwroot/js/Common.js
@@ -9,3 +9,34 @@
         toastr.error(message, 'Operation Failed', { timeOut: 5000 });
     }
 }
+
+window.ShowSwAlert = (type, message) => {
+    //if (type === "success") {
+    //    swal({          
+    //        text: message,
+    //        icon: "success"
+    //    });
+    //}
+
+    //if (type === "error") {
+    //    swal({
+    //        text: message,
+    //        icon: "error"
+    //    });
+    //}
+
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
+    }
+}
