73ca157949af328470032a044fbbbe20376b8776
jigar b. patel <jigarpatel5885@gmail.com>
2022-10-31 13:47:10 +0530

    implemented Authentication and Authorization using JWT  token for API Project

---
+++ TangyWeb_API/Controllers/AccountController.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Xml;
+using Tangy_Common;
+using Tangy_DataAccess;
+using Tangy_Models;
+using TangyWeb_API.Helper;
+
+namespace TangyWeb_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly APISettings _apiSettings;
+
+
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,             
+            RoleManager<IdentityRole> roleManager,
+            IOptions<APISettings> options)
+        {
+            _userManager = userManager;
+            
+            _signInManager = signInManager;
+            
+            _roleManager = roleManager;
+            _apiSettings = options.Value;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp([FromBody] SignUpRequestDTO signUpRequestDTO)
+        {
+            if(signUpRequestDTO==null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = signUpRequestDTO.Email,
+                Email = signUpRequestDTO.Email,
+                Name = signUpRequestDTO.Name,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, signUpRequestDTO.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(new SignUpResponseDTO()
+                {
+                    IsRegisterationSuccessful = false,
+                    Errors= result.Errors.Select(u=>u.Description)
+
+                }) ;
+            }
+
+            var roleResult = await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new SignUpResponseDTO()
+                {
+                    IsRegisterationSuccessful = false,
+                    Errors = result.Errors.Select(u => u.Description)
+
+                });
+            }
+            return StatusCode(201);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn([FromBody] SignInRequestDTO signInRequestDTO)
+        {
+            if (signInRequestDTO == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(signInRequestDTO.UserName, signInRequestDTO.Password, false,false);
+
+
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(signInRequestDTO.UserName);
+                if(user ==null)
+                {
+                    return Unauthorized(new SignInResponseDTO()
+                    {
+                        IsAuthSuccessful = false,
+                        ErrorMessage = "Invalid Authentication"
+
+                    });
+                }
+
+                // login 
+                var signinCredentials = GetSigningCredentials();
+                var claims = await GetClaims(user);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: _apiSettings.ValidIssuer,
+                    audience: _apiSettings.ValidAudience,
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(30),
+                    signingCredentials: signinCredentials);
+
+                var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+                return Ok(new SignInResponseDTO()
+                {
+                    IsAuthSuccessful = true,
+                    Token = token,
+                    UserDTO = new UserDTO()
+                    {
+                        Name = user.Name,
+                        Id = user.Id,
+                        Email = user.Email,
+                        PhoneNumber = user.PhoneNumber
+                    }
+                });
+
+            }
+            else
+            {
+                return BadRequest(new SignInResponseDTO()
+                {
+                    IsAuthSuccessful = false,
+                    ErrorMessage =  "Invalid Authentication"
+
+                });
+            }
+
+          
+
+           
+            return StatusCode(201);
+        }
+
+
+        private SigningCredentials GetSigningCredentials()
+        {
+            var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_apiSettings.SecretKey));
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+
+        }
+
+        private async Task<List<Claim>> GetClaims(ApplicationUser user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,user.Email),
+                new Claim(ClaimTypes.Email,user.Email),
+                new Claim("Id",user.Id)
+            };
+            var roles = await _userManager.GetRolesAsync(await _userManager.FindByEmailAsync(user.Email));
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;   
+        }
+
+
+    }
+}
---
+++ TangyWeb_API/Helper/APISettings.cs
@@ -0,0 +1,10 @@
+﻿namespace TangyWeb_API.Helper
+{
+    public class APISettings
+    {
+        public string SecretKey { get; set; }
+        public string ValidAudience { get; set; }
+        public string ValidIssuer { get; set; }
+
+    }
+}
